3a08def9a6db7b45df57aea3b00bb0f43b82f87c
leansantos <lesteranthony_s@yahoo.com.ph>
2015-06-21 05:39:04 +0800

    DFSDF

--- js/controller/MoviesController.js
+++
@@ -1,6 +0,0 @@
-app.controller('MoviesController', ['$scope', '$http', function($scope, $http){
-	$http.get("js/data/movies.json")
-		.success(function(response){
-			$scope.movies = response.movies;
-		});
-}])
