72eb2a301a902350a83fc6aa931a0ec630406091
leansantos <lesteranthony_s@yahoo.com.ph>
2015-06-22 04:30:13 +0800

    sdf

--- js/controller/SeriesController.js
+++
@@ -1,6 +0,0 @@
-app.controller('SeriesController', ['$scope', '$http', function($scope, $http){
-	$http.get("js/data/series.json")
-		.success(function(response){
-			$scope.tvseries = response.series;
-		});
-}])
